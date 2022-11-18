40719af21e5f5b67d0d474c26a38173918795408
João Victor Inoue <110576055+joaovictorinoue@users.noreply.github.com>
2022-11-17 22:26:33 -0300

    Continuação atividade

--- 013/app.js
+++ 013/app.js
@@ -4,6 +4,9 @@ function Id(){
     let sexo = document.getElementById('sexo').value
 
     document.getElementById('cpf').innerHTML = nome + ' ' + altura/100 + 'Metros ' + sexo
-    
+
+    document.getElementById('nome').value = ('')
+    document.getElementById('altura').value = ('')
+    document.getElementById('sexo').value = ('')
 }
 
